52c4231d7fa030722ee581a4c87159bcf12e5540
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-03-29 15:01:25 +0200

    Sass installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+<div className="hej">
+<h1>Testar</h1>
+</div>
+  </div>;
 }
 
 export default App;
