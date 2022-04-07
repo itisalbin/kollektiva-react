675881450fcd5f6357a2db3dc1f087b14d610c84
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-04-07 15:38:19 +0200

    Start on page 7

--- src/pages/createAd07.js
+++ src/pages/createAd07.js
@@ -2,6 +2,7 @@ import React from "react";
 import ProgressCreateAd from "../components /ProgressCreateAd";
 import CreateAdNextPrev from "../components /CreateAdNextPrev";
 import CreateAdMenu from "../components /CreateAdMenu";
+import Dummy from "../Assets/Images/dummy2.webp";
 
 import "./createAd.scss";
 
@@ -11,16 +12,23 @@ const CreateAd07 = () => {
       <ProgressCreateAd nr="7" />
       <div className="grid-container">
         <CreateAdMenu />
-        <div>
+        <div className="main-container">
           <h2>Förhadsgranska: Såhär ser din bostadsprofil ut</h2>
-          <div className="input-container"></div>
+          <div className="headline-container">
+            <p className="bold">Rubrik på bostaden</p>
+            <p>Villa - Hela boendet - Möblerat</p>
+            <p className="bold">15.000:- i månaden</p>
+          </div>
+          <img className="main-image" src={Dummy}></img>
+          <div className="small-image-container">
+            <img className="supporting-image" src={Dummy}></img>
+            <img className="supporting-image" src={Dummy}></img>
+            <img className="supporting-image" src={Dummy}></img>
+            <img className="supporting-image" src={Dummy}></img>
+          </div>
         </div>
       </div>
-      <CreateAdNextPrev 
-      link="/create-ad8"
-      text="Nästa"
-      backTo="/create-ad6"
-      />
+      <CreateAdNextPrev link="/create-ad8" text="Nästa" backTo="/create-ad6" />
     </div>
   );
 };
