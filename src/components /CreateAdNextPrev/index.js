d8b68383ad6e0040f6925554ed9ef610fc159c05
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-04-19 14:04:46 +0200

    Final on create-ad

--- src/components /CreateAdNextPrev/index.js
+++ src/components /CreateAdNextPrev/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./CreateAdNextPrev.scss";
+import { IoIosArrowBack } from "react-icons/io";
+
 
 const CreateAdNextPrev = ({ backTo, abort, link, text }) => {
   return (
     <div className="create-ad-next-prev">
+      <div className="back-container">
+        <IoIosArrowBack size="25px"/>
       <a href={backTo}> Tillbaka</a>
-      
+      </div>
       <div className="flex-container">
         <a className="abort" href="/">
           Avbryt
